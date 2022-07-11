2adfd8104420bc40fc1765232c6e2bcf081232e0
MysticShow <MysticShow@example.com>
2022-07-11 16:11:37 +0700

    homework_4

---
+++ 1_seminar/4_homework/Program.cs
@@ -0,0 +1,22 @@
+﻿// using System;
+
+namespace homework3 {
+
+    class Program {
+
+        static void Main() {
+            Console.Write("Please enter a number N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.Write("All even number: \n");
+            for(int i = 0; i < N; i++) {
+                if(i % 2 == 0) {
+                    Console.WriteLine("{0}", i);
+
+                }
+            }
+
+        }
+
+    }
+}
