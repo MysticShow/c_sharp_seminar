ff6fb9917ef940ecfb90fd7b5e70066149d3d24c
MysticShow <MysticShow@example.com>
2022-07-11 15:49:25 +0700

    homework_2

---
+++ 1_seminar/2_homework/Program.cs
@@ -0,0 +1,27 @@
+﻿// using System;
+
+namespace homework2 {
+
+    class Program {
+
+        static void Main() {
+            Console.Write("Please enter number one: ");
+            byte a = byte.Parse(Console.ReadLine());
+
+            Console.Write("Please enter number two: ");
+            byte b = byte.Parse(Console.ReadLine());
+
+            Console.Write("Please enter number three: ");
+            byte c = byte.Parse(Console.ReadLine());
+
+            int max = a;
+
+            if (b > a) { max = b; }
+
+            if (c > max) { max = c; }
+
+            Console.WriteLine(max);
+        }
+
+    }
+}
