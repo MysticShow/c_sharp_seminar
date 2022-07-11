3c9b52993f8a418db3a69f558e3341dd4658ad68
MysticShow <MysticShow@example.com>
2022-07-11 15:58:06 +0700

    homework_3

---
+++ 1_seminar/3_homework/Program.cs
@@ -0,0 +1,16 @@
+﻿// using System;
+
+namespace homework3 {
+
+    class Program {
+
+        static void Main() {
+            Console.Write("Please enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a % 2 == 0 ? "Yes, even number!" : "No, not an even number");
+
+        }
+
+    }
+}
