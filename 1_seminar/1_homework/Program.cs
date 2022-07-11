b97087b49ae65fdfd1d5101cf4e678c2b0808ec1
MysticShow <MysticShow@example.com>
2022-07-11 15:34:47 +0700

    homework_1

---
+++ 1_seminar/1_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+
+namespace homework1{
+
+    class Program{
+
+        static void Main(){
+            Console.Write("Please enter number one: ");
+            byte a = byte.Parse(Console.ReadLine());
+
+            Console.Write("Please enter number two: ");
+            byte b = byte.Parse(Console.ReadLine());
+
+            if (a>b) {
+                Console.WriteLine(a + " > " + b);
+
+            } else {
+                 Console.WriteLine(a + " < " + b);
+
+            }
+        }
+
+    }
+}
