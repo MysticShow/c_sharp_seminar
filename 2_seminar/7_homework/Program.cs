837441972da90aaf8027759257ea99f499b4fb94
MysticShow <MysticShow@example.com>
2022-07-18 11:30:34 +0700

    homework_7

---
+++ 2_seminar/7_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace homework7 {
+
+    class Program {
+
+        static void Main() {
+            
+            Console.Write("Please enter the day of the week: ");
+            byte n = byte.Parse(Console.ReadLine());
+
+            if(n <= 5) {
+
+                Console.Write("No. Today is not a day off =(");
+
+            } else if (n <= 7) {
+
+                Console.Write("Yes! Today is a day off!");
+
+            } else {
+                Console.Write("Enter a day from 1 to 7, please =)");
+            }
+            
+        }
+    }
+}
