42c4c73819fc160a9cc6506ffe273d14b7554e4a
MysticShow <MysticShow@example.com>
2022-07-18 11:13:47 +0700

    homework_6

---
+++ 2_seminar/6_homework/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace homework6 {
+
+    class Program {
+
+        static void Main() {
+            
+            int num = int.Parse(Console.ReadLine());
+
+             Console.WriteLine(num > 99 ? num.ToString()[2] : '-');
+             Console.ReadKey(true);
+
+        }
+     }
+}
